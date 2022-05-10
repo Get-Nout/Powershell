a1c07996ecad73e2ff1dc3786d97f68900301761
Nout <39953451+Get-Nout@users.noreply.github.com>
2022-05-10 15:35:07 +0200

    Update Clear-EID-Certificates.ps1

--- Clear-EID-Certificates.ps1
+++ Clear-EID-Certificates.ps1
@@ -1,7 +1,7 @@
 ﻿
 	$Folder = "Cert:\CurrentUser\My\"
 	$SubjectFilter = "SERIALNUMBER*"
-	$Exclusion = "*G=Bregt, SN=Vanzonhoven*"
+	$Exclusion = "*G=firstname, SN=lastname*"
 
 	foreach($Cert in (Get-childitem $Folder | Where-Object {($_.Subject -NotLike $Exclusion) -and ($_.Subject -Like $SubjectFilter)})){
 		Write-Host "Removing File: " $Cert.Subject.Substring(($cert.Subject.IndexOf(",") +2),($Cert.Subject.length - $cert.Subject.IndexOf(",") -2))
